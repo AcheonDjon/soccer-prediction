d0f358271b0c815269956c73d9ac54f513745ada
Jonathan Ramachandran <jramachandran30@mtlstudents.net>
2024-02-28 12:34:36 -0500

    saving model xgjonathan

--- src/xgjonathan.py
+++ src/xgjonathan.py
@@ -1,6 +1,8 @@
 import pandas as pd
-import xgboost
+import xgboost as xg 
 from sklearn.model_selection import train_test_split 
+from sklearn.metrics import accuracy_score 
+
 
 df = pd.read_csv('./data/regular_season_data.csv')
 
@@ -13,13 +15,27 @@ def outcome(row):
             return 1
         else:
             return 0 
+        
     else:
         return 0
 
-df['Outcome'] = df.apply(outcome, axis=1)
+X = df.iloc [:,5:]
 
-print (df[['Outcome','HomeScore']])
+Y = df.apply(outcome, axis=1)
 
-# X = df[[outcome]]
-# Y = df[[]]
+
+
+
+X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
+
+classifier = xg.XGBClassifier()
+
+classifier.fit(X_train, y_train)
+
+y_pred = classifier.predict(X_test)
+
+print(y_pred)
+accuracy = accuracy_score(y_test, y_pred)
+
+print(accuracy)
 # df = df.drop(['game_id',"HomeTeam","AwayTeam","HomeScore",'AwayScore'], axis=1)
